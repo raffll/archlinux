1bd2a567330676d8ae11dced0f5c7487c0bdc024
raffll <rafal.wierzchos@gmail.com>
2016-09-16 15:49:13 +0200

    update 16.09

--- archlinux-backup.sh
+++ archlinux-backup.sh
@@ -3,23 +3,10 @@
 DATE=$(date +"%d-%m-%Y")
 DIR=/home/backup
 
-tar -cjvf $DIR/etc-backup-$DATE.tar.bz2 /etc
-tar -cjvf $DIR/pacman-database-backup-$DATE.tar.bz2 /var/lib/pacman/local
-tar -cjvf $DIR/$USER-backup-$DATE.tar.bz2 \
-			--exclude='.wine' \
-			--exclude='.cache' \
-			--exclude='.cabal' \
-			--exclude='Steam' \
-			--exclude='Trash' \
-			--exclude='openmw' \
-			--exclude='chromium' \
-			--exclude='.kodi' \
-			--exclude='.ccache' \
-			--exclude='.mozilla' \
-			--exclude='.IdeaIC14' \
-			--exclude='.log' \
-			--exclude='.thumbnails' \
-			--exclude='unity3d' \
-			$HOME/.[^.]*
+mkdir $DIR/$DATE
 
-pacman -Qe | awk '{print $1}' | sed ':a;N;$!ba;s/\n/ /g' > $DIR/package_list.txt
+tar -cjvf $DIR/$DATE/etc-backup-$DATE.tar.bz2 /etc
+tar -cjvf $DIR/$DATE/pacman-database-backup-$DATE.tar.bz2 /var/lib/pacman/local
+tar -cjvf $DIR/$DATE/$USER-backup-$DATE.tar.bz2 -X backup-exclude.txt $HOME/.[^.]*
+
+pacman -Qe | awk '{print $1}' | sed ':a;N;$!ba;s/\n/ /g' > $DIR/$DATE/package-list-$DATE.txt
--- archlinux-clean.sh
+++ archlinux-clean.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-sudo echo -e "\e[31m\e[1mCreate themes symlinks...\e[0m"
 sudo find -L /usr/share/themes/ -maxdepth 1 -type l -delete
 sudo find -L /usr/share/icons/ -maxdepth 1 -type l -delete
 sudo ln -s $HOME/.themes/* /usr/share/themes/
 sudo ln -s $HOME/.icons/* /usr/share/icons/
 
-echo -e "\e[31m\e[1mEmpty recycle bin...\e[0m"
 sudo rm -vr $HOME/.local/share/Trash/files/*
 sudo rm -vr $HOME/.local/share/Trash/expunged/*
 sudo rm -vr $HOME/.local/share/Trash/info/*
@@ -15,14 +13,12 @@ sudo rm -vr /"\$RECYCLE.BIN"
 sudo rm -vr /home/"\$RECYCLE.BIN"
 sudo rm -vr /var/cache/pacman/pkg/*.xz.part
 
-echo -e "\e[31m\e[1mClean wine files...\e[0m"
 rm -vr $HOME/.local/share/applications/wine-extension*
 rm -vr $HOME/.local/share/applications/wine
 rm -vr $HOME/.local/share/desktop-directories/
-rm -vr $HOME/.local/share/icons &>/dev/null
+rm -vr $HOME/.local/share/icons
 rm -vr $HOME/.config/menus/applications-merged
 
-echo -e "\e[31m\e[1mClean unnecessary files...\e[0m"
 rm -vr $HOME/.config/xfce4/xfce4-notes.gtkrc*
 rm -vr $HOME/.config/xfce4/panel/xfce4-notes-plugin*
 
