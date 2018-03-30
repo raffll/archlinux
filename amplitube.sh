aa3014016dd51cee8b9c842038f7abd0b4cdd760
raffll <rafal.wierzchos@gmail.com>
2018-03-30 16:28:48 +0200

    update

--- amplitube.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PREFIX="$HOME/.local/share/wineprefixes/AmpliTube 3"
-
-env WINEPREFIX="$PREFIX" vsthost "$PREFIX/drive_c/Program Files/VstPlugIns/AmpliTube 3.dll"
--- archlinux-update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yaourt -Syua && sudo paccache -ruk1 && sudo pacman-optimize
